637488a446fad55c6b65d240602e770a6992331e
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2023-06-23 09:02:59 +0700

    Update

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-var x = (document.getElementById("audio1").autoplay = true);
-$(document).ready(function () {
-  $("#modal1").modal("show");
-});
-function lanjut2() {
-  //   $("#audio1").get(0).play();
-  $("#modal1").modal("hide");
-  $("#modal2").modal("show");
-}
